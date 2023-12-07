bfb91e3c832c08e5283ca4914cf347960f65bad1
Larissa <10475169697@estudante.ufjf.br>
2023-12-07 15:58:10 -0300

    limpa formulário

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
+document.entrada.reset();
+
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('buttonLimparLista').addEventListener('click', () => {
