e63cc2f53f05b394ba3a97009ca208238812766b
Larissa <10475169697@estudante.ufjf.br>
2023-12-06 21:45:10 -0300

    cria main.js e acessa estoque

---
+++ main.js
@@ -0,0 +1,8 @@
+import { getEstoque, transacaoNoEstoque } from "./estoque.js";
+
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria")
+
+document.entrada.addEventListener('submit', leFormulario);
+
+atualizaTela();
